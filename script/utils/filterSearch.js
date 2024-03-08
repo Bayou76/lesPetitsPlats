6d94ab15baec4d059574989dbedab07f9eafa0b0
bayous3 <sebia.baya@outlook.com>
2024-03-08 14:40:08 +0100

    maj fonction recherche

--- script/utils/filterSearch.js
+++ script/utils/filterSearch.js
@@ -1,33 +1,70 @@
 function filterAndDisplayRecipes() {
     // Obtention de la valeur de recherche
-    const searchTerm = document.getElementById('search-input').value.trim().toLowerCase();
+    const searchTerm = document
+        .getElementById("search-input")
+        .value.trim()
+        .toLowerCase();
 
     // Récupération des éléments sélectionnés dans les tags
-    const selectedIngredients = Array.from(document.querySelectorAll(".selected-ingredient span")).map(item => item.textContent.trim().toLowerCase());
-    const selectedAppliances = Array.from(document.querySelectorAll(".selected-appliance span")).map(item => item.textContent.trim().toLowerCase());
-    const selectedUstensils = Array.from(document.querySelectorAll(".selected-utensil span")).map(item => item.textContent.trim().toLowerCase());
+    const selectedIngredients = Array.from(
+        document.querySelectorAll(".selected-ingredient span")
+    ).map((item) => item.textContent.trim().toLowerCase());
+    const selectedAppliances = Array.from(
+        document.querySelectorAll(".selected-appliance span")
+    ).map((item) => item.textContent.trim().toLowerCase());
+    const selectedUstensils = Array.from(
+        document.querySelectorAll(".selected-utensil span")
+    ).map((item) => item.textContent.trim().toLowerCase());
 
     // Effacer les recettes actuellement affichées
     const recipesListDiv = document.getElementById("recipes-list");
-    recipesListDiv.innerHTML = '';
+    recipesListDiv.innerHTML = "";
 
     // Tableau pour stocker les recettes filtrées
-    const filteredRecipeCards = recipes.filter(recipe => {
-        const recipeTitle = recipe.name.toLowerCase();
-        const recipeDescription = recipe.description.toLowerCase();
-        const recipeIngredients = recipe.ingredients.map(ing => ing.ingredient.toLowerCase());
+    let filteredRecipeCards = [];
 
-        const matchesSearchTerm = recipeTitle.includes(searchTerm) || recipeDescription.includes(searchTerm) || recipeIngredients.some(ingredient => ingredient.includes(searchTerm));
+    // Si la longueur de la chaîne de recherche est inférieure à 3, afficher toutes les recettes
+    if (searchTerm.length < 3) {
+        // Créer les cartes de recette pour toutes les recettes disponibles
+        filteredRecipeCards = recipes.map((recipeData) =>
+            new RecipeCard(new Recipe(recipeData)).createRecipeCard()
+        );
+    } else {
+        // Filtrer les recettes en fonction de la recherche et des éléments sélectionnés
+        filteredRecipeCards = recipes
+            .filter((recipe) => {
+                const recipeTitle = recipe.name.toLowerCase();
+                const recipeDescription = recipe.description.toLowerCase();
+                const recipeIngredients = recipe.ingredients.map((ing) =>
+                    ing.ingredient.toLowerCase()
+                );
 
-        const matchesSelectedTags = selectedIngredients.every(ingredient => recipeIngredients.includes(ingredient)) &&
-            (selectedAppliances.length === 0 || selectedAppliances.includes(recipe._appliance.toLowerCase())) &&
-            selectedUstensils.every(ustensil => recipe.ustensils.map(u => u.toLowerCase()).includes(ustensil));
+                const matchesSearchTerm =
+                    recipeTitle.includes(searchTerm) ||
+                    recipeDescription.includes(searchTerm) ||
+                    recipeIngredients.some((ingredient) =>
+                        ingredient.includes(searchTerm)
+                    );
 
-        return matchesSearchTerm && matchesSelectedTags;
-    }).map(recipeData => new RecipeCard(new Recipe(recipeData)).createRecipeCard());
+                const matchesSelectedTags =
+                    selectedIngredients.every((ingredient) =>
+                        recipeIngredients.includes(ingredient)
+                    ) &&
+                    (selectedAppliances.length === 0 ||
+                        selectedAppliances.includes(recipe._appliance.toLowerCase())) &&
+                    selectedUstensils.every((ustensil) =>
+                        recipe.ustensils.map((u) => u.toLowerCase()).includes(ustensil)
+                    );
+
+                return matchesSearchTerm && matchesSelectedTags;
+            })
+            .map((recipeData) =>
+                new RecipeCard(new Recipe(recipeData)).createRecipeCard()
+            );
+    }
 
     // Ajouter les cartes de recette filtrées à la div recipes-list
-    filteredRecipeCards.forEach(card => {
+    filteredRecipeCards.forEach((card) => {
         recipesListDiv.appendChild(card);
     });
 
