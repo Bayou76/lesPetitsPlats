1567c702df2662a475552f259f4b89298ba9de77
bayous3 <sebia.baya@outlook.com>
2024-03-08 14:26:51 +0100

    maj pour filtre recherche

--- script/utils/filterSearch.js
+++ script/utils/filterSearch.js
@@ -1,35 +1,53 @@
 function filterAndDisplayRecipes() {
-    // Obtention de la valeur de recherche
-    const searchTerm = document.getElementById('search-input').value.trim().toLowerCase();
-    // Récupération des éléments sélectionnés dans les tags
-    const selectedIngredients = [];
-    const selectedIngredientsSpans = document.querySelectorAll(".selected-ingredient span");
-    for (let i = 0; i < selectedIngredientsSpans.length; i++) {
-        const item = selectedIngredientsSpans[i];
-        selectedIngredients.push(item.textContent.trim().toLowerCase());
+  // Obtention de la valeur de recherche
+  const searchTerm = document
+    .getElementById("search-input")
+    .value.trim()
+    .toLowerCase();
+
+  // Récupération des éléments sélectionnés dans les tags
+  const selectedIngredients = [];
+  const selectedIngredientsSpans = document.querySelectorAll(
+    ".selected-ingredient span"
+  );
+  for (let i = 0; i < selectedIngredientsSpans.length; i++) {
+    const item = selectedIngredientsSpans[i];
+    selectedIngredients.push(item.textContent.trim().toLowerCase());
+  }
+
+  const selectedAppliances = [];
+  const selectedAppliancesSpans = document.querySelectorAll(
+    ".selected-appliance span"
+  );
+  for (let i = 0; i < selectedAppliancesSpans.length; i++) {
+    const item = selectedAppliancesSpans[i];
+    selectedAppliances.push(item.textContent.trim().toLowerCase());
+  }
+
+  const selectedUstensils = [];
+  const selectedUstensilsSpans = document.querySelectorAll(
+    ".selected-utensil span"
+  );
+  for (let i = 0; i < selectedUstensilsSpans.length; i++) {
+    const item = selectedUstensilsSpans[i];
+    selectedUstensils.push(item.textContent.trim().toLowerCase());
+  }
+
+  // Effacer les recettes actuellement affichées
+  const recipesListDiv = document.getElementById("recipes-list");
+  recipesListDiv.innerHTML = "";
+
+  // Tableau pour stocker les recettes filtrées
+  const filteredRecipeCards = [];
+
+  if (searchTerm.length < 3) {
+    for (let i = 0; i < recipes.length; i++) {
+        const recipeData = recipes[i];
+        const recipe = new Recipe(recipeData);
+        const recipeCard = new RecipeCard(recipe);
+        filteredRecipeCards.push(recipeCard.createRecipeCard());
     }
-
-    const selectedAppliances = [];
-    const selectedAppliancesSpans = document.querySelectorAll(".selected-appliance span");
-    for (let i = 0; i < selectedAppliancesSpans.length; i++) {
-        const item = selectedAppliancesSpans[i];
-        selectedAppliances.push(item.textContent.trim().toLowerCase());
-    }
-
-    const selectedUstensils = [];
-    const selectedUstensilsSpans = document.querySelectorAll(".selected-utensil span");
-    for (let i = 0; i < selectedUstensilsSpans.length; i++) {
-        const item = selectedUstensilsSpans[i];
-        selectedUstensils.push(item.textContent.trim().toLowerCase());
-    }
-
-    // Effacer les recettes actuellement affichées
-    const recipesListDiv = document.getElementById("recipes-list");
-    recipesListDiv.innerHTML = '';
-
-    // Tableau pour stocker les recettes filtrées
-    const filteredRecipeCards = [];
-
+} else {
     // Parcourir toutes les recettes et vérifier les correspondances
     for (let i = 0; i < recipes.length; i++) {
         const recipeData = recipes[i];
@@ -57,12 +75,13 @@ function filterAndDisplayRecipes() {
             filteredRecipeCards.push(recipeCard.createRecipeCard());
         }
     }
+}
 
-    // Ajouter les cartes de recette filtrées à la div recipes-list
-    for (let i = 0; i < filteredRecipeCards.length; i++) {
-        recipesListDiv.appendChild(filteredRecipeCards[i]);
-    }
+  // Ajouter les cartes de recette filtrées à la div recipes-list
+  for (let i = 0; i < filteredRecipeCards.length; i++) {
+    recipesListDiv.appendChild(filteredRecipeCards[i]);
+  }
 
-    // Mettre à jour le nombre de recettes affichées
-    app.updateRecipeCount(filteredRecipeCards.length);
+  // Mettre à jour le nombre de recettes affichées
+  app.updateRecipeCount(filteredRecipeCards.length);
 }
